53c4a2038c912515fb18783c1459b266508ddd99
ashokyak <kumarashok092@gmail.com>
2014-11-11 13:00:57 +0530

    first commit

---
+++ src/com/qajenkins/phase1/Phase1.java
@@ -0,0 +1,13 @@
+package com.qajenkins.phase1;
+
+import java.util.Scanner;
+
+public class Phase1 {
+	public static void main(String[] args) {
+		Scanner inp=new Scanner(System.in);
+		System.out.println("Enter a string:");
+		String msg = inp.nextLine();
+		System.out.println("String entered is : "+msg);
+	}
+	
+}
